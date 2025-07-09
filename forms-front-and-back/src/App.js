54a2509d1ea4a5a84dbf5b2ea47724809a2cb6ea
Gabriel <gabriel_lorenssetti@estudante.sesisenai.org.br>
2025-07-09 12:31:08 -0300

    upload codigo base

---
+++ forms-front-and-back/src/App.js
@@ -0,0 +1,16 @@
+import './index.css';
+import Forms from './components/forms'
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="Align">
+        <Forms />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ forms-front-and-back/src/components/forms.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Forms () {
+    return(
+        <div>
+            <section className='Container'>
+                <header>
+                    <div className='balls'>
+                        <div className='ball1'></div>
+                        <div className='ball2'></div>
+                        <div className='ball3'></div>
+                    </div>
+
+                    <h1>Formulário Cadastro</h1>
+                </header>
+            </section>
+        </div>
+    );
+}
+
+export default Forms;
